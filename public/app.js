d452772fa8323323523b639a8f449061077d848a
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-09-05 03:44:17 -0700

    step 3.1 created getMealTime() a helper function to check time of day and return meal time info

--- public/app.js
+++ public/app.js
@@ -11,7 +11,16 @@ console.log(userTime())
 
 // helper functions
 // check time of day
+function getMealTime() {
+    const tod = userTime()
+    // if (tod > 20) {return 'late-night snack'}
+    // else if (tod > 16) {return 'dinner'}
+    // else if (tod > 11) {return 'lunch'}
+    // else {return 'breakfast'}
+    return tod > 20 ? 'late night snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast'
+}
 
+console.log(getMealTime())
 
 // build ads
 // build ad 1
